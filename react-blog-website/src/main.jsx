61d5b77e17b0235b61c1164958c7709a8c611682
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-10-11 14:06:24 +0100

    final project

---
+++ react-blog-website/src/main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/Blogs",
+        element: <Blogs/>
+      },
+      {
+        path: "/About",
+        element: <About/> 
+      },
+      {
+        path: "/Contact",
+        element: <Contact/>
+      },
+      {
+        path: "/Services",
+        element: <Services/>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
